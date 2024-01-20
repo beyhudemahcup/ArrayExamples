499936d508b0b2f8a8b6c04c5771117501dc00f3
beyhudemahcup <beyhudemahcup@gmail.com>
2024-01-20 15:12:40 +0100

    fibonacci sequance and array examples are added

---
+++ ArrayExamples/ParamsAverage.cs
@@ -0,0 +1,35 @@
+﻿namespace ArrayExamples
+{
+    public static class ParamsAverage
+    {
+        public static double Average(bool willBePrint, params double[] arr)
+        {
+            var sum = 0.0;
+
+            foreach ( var i in arr )
+            {
+                sum += i;
+            }
+
+            if ( sum == 0.0)
+            {
+                PrintAverage(0.0);
+                return 0.0;
+            }
+
+            double average = sum / arr.Length;
+            
+            if (willBePrint)
+            {
+                PrintAverage(average);
+            }
+            
+            return average;
+        }
+
+        private static void PrintAverage(double average)
+        {
+            Console.WriteLine("Average of array from ParamsAverage class is: " + average.ToString("0.00"));
+        }
+    }
+}
---
+++ FibonacciSequence/Calculator.cs
@@ -0,0 +1,29 @@
+﻿namespace FibonacciSequence
+{
+    public static class Calculator
+    {
+       public static void FibonacciCalculator(int inputNumber)
+        {
+            //fibonacci sequence
+            int[] fibsq = new int[inputNumber];
+
+            fibsq[0] = 1;
+            fibsq[1] = 1;
+
+            Console.WriteLine("Its preparing...");
+
+            //adding some fun
+            Thread.Sleep(1000);
+
+            for (int i = 2; i < fibsq.Length; i++)
+            {
+                fibsq[i] = fibsq[i - 1] + fibsq[i - 2];
+            }
+
+            foreach (int i in fibsq)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
